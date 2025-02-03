0d38963bfe77a0bb0a29b45185cd0e6efeecb9b3
the-romantic-dev <ahxplay@gmail.com>
2025-02-03 23:10:56 +0300

    change structure

--- sympy_prettifier/__init__.py
+++
@@ -1,4 +0,0 @@
-from sympy_prettifier.latex.rational import rational_to_string, rational_to_latex
-from sympy_prettifier.omml.math.braces import braces
-from sympy_prettifier.omml.math.matrix import matrix_from_elements, matrix_from_sympy
-from sympy_prettifier.omml.math.sup import sup
